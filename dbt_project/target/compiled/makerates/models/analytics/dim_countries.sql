1edb0cd2458faafd18c56220610472a2f31f4369
Manos Palaiologos <manos@Manoss-MacBook-Pro.local>
2026-01-29 15:08:01 +0100

    Track deletes and stop tracking dbt_project/dbt_packages/ and dbt_project/target/ (ignore them)

--- dbt_project/target/compiled/makerates/models/analytics/dim_countries.sql
+++
@@ -1,8 +0,0 @@
-
-
-SELECT
-    UPPER(country_code) as country_code,
-    country_name,
-    UPPER(currency_code) as currency_code,
-    region
-FROM "analytics"."main_analytics"."country_currencies"
